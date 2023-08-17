4d64f8fa8bdba28a06507e5d363105b242485bdd
Nicolas Gte <63913554+nicolas-gte@users.noreply.github.com>
2023-08-17 10:18:32 +0200

    Update Main.py
    Added messages of start and end of program

--- Main.py
+++ Main.py
@@ -4,8 +4,10 @@ from Sender import EnvoiMail
 from config import VariablesExcelToPython,VariablesEnvoiMail,mailDuSuperviseur,PeriodeEntretienenMois
 
 if __name__=="__main__":
+    print("---Lancement du programme...---")
     ExcelToPython(VariablesExcelToPython)
     VariablesEnvoiMail["html_content"], VariablesEnvoiMail["date"], envoyerMail, envoiSuperviseur = create_html_content(VariablesExcelToPython["nom_fichierj"],PeriodeEntretienenMois)
     if envoiSuperviseur: VariablesEnvoiMail["destinataire"].append(mailDuSuperviseur) # S'il faut prévenir le superviseur, on ajoute son mail aux destinataires
     print("ENVOYÉ AU SUPERVISEUR:", envoiSuperviseur)
     if envoyerMail: EnvoiMail(VariablesEnvoiMail) # On envoie le mail si le script "Editor.py" (avec la fonction create_html_content()) l'a jugé nécessaire
+    print("---Fin du programme---")
